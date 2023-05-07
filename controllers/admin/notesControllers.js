734c1d3e5adaa641dce67f1ee1133bedf9392999
Jamshaid Javaid <jamshaidjavaid360@gmail.com>
2023-05-07 18:11:08 +0500

    changes in notes controllers

--- controllers/admin/notesControllers.js
+++ controllers/admin/notesControllers.js
@@ -11,12 +11,8 @@ const getNotes = async (req, res, next) => {
   try {
     if (role === "Admin") {
       notes = await Note.find().sort({ createdAt: -1 });
-    } else if (role === "Student") {
-      const student = await Student.findById(userId);
-      notes = student.notes;
-    } else if (role === "Teacher") {
-      const teacher = await Teacher.findById(userId);
-      notes = teacher.notes;
+    } else {
+      new HttpError("Something went wrong, role is not defined", 500);
     }
     res.json({ notes: notes.map((n) => n.toObject({ getters: true })) });
   } catch (err) {
@@ -29,37 +25,19 @@ const getNotes = async (req, res, next) => {
 
 // CREATING A NOte
 const createNote = async (req, res, next) => {
-  const { note, userId, role } = req.body;
+  const { note, role } = req.body;
 
   const session = await mongoose.startSession();
   session.startTransaction();
 
   try {
-    if (role === "Student") {
-      const student = await Student.findById(userId);
-      if (!student) {
-        return next(new HttpError("This student doesn't exist", 404));
-      }
-      const createdNote = {
-        note,
-      };
-      student.notes.push(createdNote);
-      await student.save({ session });
-    } else if (role === "Teacher") {
-      const teacher = await Teacher.findById(userId);
-      if (!teacher) {
-        return next(new HttpError("This teacher doesn't exist", 404));
-      }
-      const createdNote = {
-        note,
-      };
-      teacher.notes.push(createdNote);
-      await teacher.save({ session });
-    } else if (role === "Admin") {
+    if (role === "Admin") {
       const createdNote = new Note({
         note,
       });
       await createdNote.save({ session });
+    } else {
+      new HttpError("Something went wrong, role is not defined", 500);
     }
 
     await session.commitTransaction();
@@ -79,32 +57,16 @@ const createNote = async (req, res, next) => {
 
 //  delete A NOte
 const deleteNote = async (req, res, next) => {
-  const { userId, role } = req.query;
   const { noteId } = req.params;
   const session = await mongoose.startSession();
   session.startTransaction();
 
   try {
-    if (role === "Student") {
-      await Student.updateOne(
-        { _id: userId },
-        { $pull: { notes: { _id: noteId } } },
-        { session }
-      );
-    } else if (role === "Teacher") {
-      await Teacher.updateOne(
-        { _id: userId },
-        { $pull: { notes: { _id: noteId } } },
-        { session }
-      );
-    } else if (role === "Admin") {
-      await Note.findByIdAndDelete(noteId, { session });
-    }
-
+    await Note.findByIdAndDelete(noteId, { session });
     await session.commitTransaction();
     session.endSession();
     res.json({
-      message: "Deleted Successfully",
+      message: "Deleted note Successfully",
     });
   } catch (error) {
     console.error(error);
